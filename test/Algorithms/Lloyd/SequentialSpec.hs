ca0b4054aa24dcca350fb5f11544306be3f3caf7
vi <me@vikramverma.com>
2014-06-02 16:54:17 +0800

    More general cluster-size invariance test, this one fails!

--- test/Algorithms/Lloyd/SequentialSpec.hs
+++ test/Algorithms/Lloyd/SequentialSpec.hs
@@ -1,24 +1,32 @@
 module Algorithms.Lloyd.SequentialSpec (spec, y) where
 
-import Prelude hiding (take, zipWith, length, head)
+import Prelude hiding (take, zipWith, length)
 import Algorithms.Lloyd.Sequential (Point(..), Cluster(..), step)
+import Control.Applicative ((<$>), (<*>))
 import Data.Metric (Euclidean(..))
 import Data.Random.Normal (mkNormals)
-import Data.Vector (Vector, generate, fromList, take, zipWith, length, head)
-import Test.Hspec (Spec(..), describe, it, shouldBe)
+import Data.Vector (Vector, generate, fromList, take, zipWith, length)
+import Test.Hspec (Spec(..), describe, it)
+import Test.QuickCheck (property, Arbitrary(..), choose)
 
-points :: Vector Point
-points = generate 2000 $ \n -> Point $ fromList [normals !! n, normals !! (n*2)]
-  where normals = mkNormals 0x29a
+data Structure = Structure
+  { points   :: Vector Point
+  , clusters :: Vector Cluster
+  } deriving (Show)
 
-clusters :: Vector Cluster
-clusters = zipWith Cluster (fromList [0..2]) (take 3 points)
+instance Arbitrary Structure where
+  arbitrary = do
+    normals <- mkNormals <$> arbitrary
+    (a1, k) <- (,) <$> choose (1,200) <*> choose (1,200)
+    let points   = generate a1 $ \n -> Point $ fromList [normals !! n, normals !! (n*2)]
+        clusters = zipWith Cluster (fromList [0..k-1]) (take k points)
+    return $ Structure points clusters
 
 spec :: Spec
-spec = 
-  describe "step" . it "Cluster length should be invariant upon repeated application." $ do
+spec = do
+  describe "step" . it "Cluster length should be invariant upon repeated application." . property $ \(Structure points clusters) -> do
     let fixedPoint = y (step Euclidean `flip` points) clusters
-    length clusters `shouldBe` length fixedPoint
+    length clusters == length fixedPoint
 
 y :: Eq a => (a -> a) -> a -> a
 y f x = if x == f x then x else y f (f x)
--- test/Algorithms/Lloyd/StrategiesSpec.hs
+++ test/Algorithms/Lloyd/StrategiesSpec.hs
@@ -3,21 +3,29 @@ module Algorithms.Lloyd.StrategiesSpec (spec) where
 import Prelude hiding (take, zipWith, length, head)
 import Algorithms.Lloyd.Strategies (Point(..), Cluster(..), step)
 import Algorithms.Lloyd.SequentialSpec (y)
+import Control.Applicative ((<$>),(<*>))
 import Data.Metric (Euclidean(..))
 import Data.Random.Normal (mkNormals)
 import Data.Vector (Vector, generate, fromList, take, zipWith, length, head)
 import Data.Vector.Split (chunksOf)
-import Test.Hspec (Spec(..), describe, it, shouldBe)
+import Test.Hspec (Spec(..), describe, it)
+import Test.QuickCheck (property, Arbitrary(..), choose)
 
-points :: Vector (Vector Point)
-points = chunksOf 30 . generate 2000 $ \n -> Point $ fromList [normals !! n, normals !! (n*2)]
-  where normals = mkNormals 0x29a
+data Structure = Structure
+  { points   :: Vector (Vector Point)
+  , clusters :: Vector Cluster
+  } deriving (Show)
 
-clusters :: Vector Cluster
-clusters = zipWith Cluster (fromList [0..2]) (take 3 $ head points)
+instance Arbitrary Structure where
+  arbitrary = do
+    normals   <- mkNormals <$> arbitrary
+    (a0,a1,k) <- (,,) <$> choose (1,200) <*> choose (1,200) <*> choose (1,200)
+    let points   = chunksOf a0 . generate a1 $ \n -> Point $ fromList [normals !! n, normals !! (n*2)]
+        clusters = zipWith Cluster (fromList [0..k-1]) (take k $ head points)
+    return $ Structure points clusters
 
 spec :: Spec
-spec = 
-  describe "step" . it "Cluster length should be invariant upon repeated application." $ do
+spec = do
+  describe "step" . it "Cluster length should be invariant upon repeated application." . property $ \(Structure points clusters) -> do
     let fixedPoint = y (step Euclidean `flip` points) clusters
-    length clusters `shouldBe` length fixedPoint
+    length clusters == length fixedPoint
